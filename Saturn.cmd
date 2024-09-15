9f4ee01b560c3f35b97cb64366793035a7c0f39d
mystic007-real <jaymo3491@gmail.com>
2024-09-14 22:34:52 -0400

    Delete Saturn.cmd

--- Saturn.cmd
+++
@@ -1,7 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'Saturn.exe' with Python DLL being found.
-set PATH=C:\Users\jaymo\AppData\Local\Programs\Python\PYTHON~1;%PATH%
-set PYTHONHOME=C:\Users\jaymo\AppData\Local\Programs\Python\Python312
-set NUITKA_PYTHONPATH=C:\Users\jaymo\OneDrive\Desktop\Saturn;C:\Users\jaymo\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\jaymo\AppData\Local\Programs\Python\Python312\Lib;C:\Users\jaymo\AppData\Local\Programs\Python\Python312;C:\Users\jaymo\AppData\Local\Programs\Python\Python312\Lib\site-packages;C:\Users\jaymo\AppData\Local\Programs\Python\Python312\Lib\site-packages\win32;C:\Users\jaymo\AppData\Local\Programs\Python\Python312\Lib\site-packages\win32\lib;C:\Users\jaymo\AppData\Local\Programs\Python\Python312\Lib\site-packages\Pythonwin;C:\Users\jaymo\AppData\Local\Programs\Python\Python312\Lib\site-packages\setuptools\_vendor
-"%~dp0Saturn.exe" %*
